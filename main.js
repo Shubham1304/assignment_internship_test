e0df650c6dc3e58ee10acd7045767b6707d5ed73
Shubham <shubhams3013@gmail.com>
2020-05-24 01:55:25 +0530

    Final commit, closing project phewww

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-var x = document.getElementById("demo");   // Get the element with id="demo"
-x.style.color = "white";
-x.style.size = 10;
+window.onload = function() {
+  document.getElementById('p1').style.display = 'block';
+  document.getElementById('p2').style.display = 'none';
+  document.getElementById('p3').style.display = 'none';
+  document.getElementById('p4').style.display = 'none';
+}
+function filllist(val){
+if(val === 'personalize'){
+  document.getElementById("p1").style.display="block";
+  document.getElementById('p2').style.display = 'none';
+  document.getElementById('p3').style.display = 'none';
+  document.getElementById('p4').style.display = 'none';
+}
+else if(val === "promote"){
+  document.getElementById("p2").style.display="block";
+  document.getElementById('p1').style.display = 'none';
+  document.getElementById('p3').style.display = 'none';
+  document.getElementById('p4').style.display = 'none';
+}
+else if(val === "reduce"){
+  document.getElementById("p3").style.display="block";
+  document.getElementById('p1').style.display = 'none';
+  document.getElementById('p2').style.display = 'none';
+  document.getElementById('p4').style.display = 'none';
+}
+else if(val === "prescribe"){
+  document.getElementById("p4").style.display="block";
+  document.getElementById('p1').style.display = 'none';
+  document.getElementById('p2').style.display = 'none';
+  document.getElementById('p3').style.display = 'none';
+}
+}
